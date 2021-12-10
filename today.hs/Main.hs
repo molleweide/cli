324e42cf68a87530d02c32b55a176c008941ac40
hjalmar jakobsson <hjalmar.jakobsson@telia.com>
2021-12-10 08:36:41 -0500

    save

---
+++ today.hs/Main.hs
@@ -0,0 +1,7 @@
+import Today (getCurrentTimestamp, getDayFromTimestamp)
+
+main :: IO ()
+main = do
+    timestamp <- getCurrentTimestamp
+    putStrLn
+        (show $ getDayFromTimestamp timestamp)
